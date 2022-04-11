84edf0dcb8bb60ccd4eab70ea657258eab4b6123
tsai_ray <silentice1534@gmail.com>
2022-04-11 22:13:10 +0800

    feat: update

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,23 @@
+import '@/styles/index.scss';
 import '@/template/index.html';
 
-import '@/styles/index.scss';
-
 import Alpine from 'alpinejs';
+import lineData from '../testData/test1.js';
+
 window.Alpine = Alpine;
+
+const { userStatistics, totalStatistics, averageStatistics } = lineData;
+
+const calcMinSec = (seconds) => {
+  const mins = Math.floor(seconds / 60);
+  const secs = Math.floor(seconds % 60);
+
+  return `${mins}分${secs}秒`;
+};
+
+Alpine.data('mainData', () => ({
+  totalMessage: totalStatistics.message,
+  totalCall: calcMinSec(totalStatistics.totalCallSeconds),
+}));
 
 Alpine.start();
